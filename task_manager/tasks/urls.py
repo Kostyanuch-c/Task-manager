71cbb811651eb19afe45d4c898814cd894ced9ba
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-09-11 13:28:40 +0500

    Edit main page for login users

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from task_manager.tasks import views
 
 urlpatterns = [
-    path('statuses', views.statuses, name='statuses'),
+    path('statuses/', views.statuses, name='statuses'),
 
 ]
