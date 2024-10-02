97a15a71a41df015095314dc0732e6e011c7112f
Konstantin Kiselev <konstantin.kiselev.1998@mail.ru>
2024-10-02 12:29:20 +0500

    Edit settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -66,7 +66,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "rollbar.contrib.django.middleware.RollbarNotifierMiddleware",
 ]
 
